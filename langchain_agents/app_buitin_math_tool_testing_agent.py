6501e784faabf6b95e78880c4fa9fe69f578caff
batuhansaylam26 <batuhansaylam1881@gmail.com>
2025-07-10 14:07:49 +0300

    math_tool

--- langchain_agents/app_buitin_math_tool_testing_agent.py
+++ langchain_agents/app_buitin_math_tool_testing_agent.py
@@ -1,6 +1,6 @@
 from dotenv import find_dotenv, load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.agents import Tool
+from langchain.agents import Tool, initialize_agent, load_tools
 from langchain.chains.llm_math.base import LLMMathChain
 
 
@@ -20,10 +20,53 @@ math_tool = Tool(
     description="Useful for when you need to answer questions related to Math."
 )
 
-tools =[
-    math_tool
-]
-print(
+tools = load_tools(
+    ['llm-math'],
+    llm=chat
+)
+"""print(
     tools[0].name,
     tools[0].description
+)"""
+
+agent = initialize_agent(
+    agent="zero-shot-react-description",
+    tools=tools,
+    llm=chat,
+    verbose=True,
+    max_iterations=3 #to avoid high bills from the LLM
 )
+query = "What is 3.1^2.1"
+result = agent.invoke(query)
+
+
+print(
+    result['output']
+)
+
+query2 = "If James is currently 45 years old, how old will he be in 50 years? \
+    If he has 4 kids and adopted 7 more, how many children does he have?"
+result2 = agent.invoke(query2)
+
+
+print(
+    result2['output']
+)
+
+query2 = "What is the capital of Mozambique"
+result2 = agent.invoke(query2)
+
+
+print(
+    result2['output']
+)
+# last query gives an error as output
+#raised error: invalid syntax (<expr>, line 1). Please try again with a valid numerical expression
+"""
+> Entering new AgentExecutor chain...
+I don't have the tools to answer factual questions about geography. I am an AI assistant that can only use the calculator tool.
+Final Answer: I am sorry, I cannot answer this question.
+
+> Finished chain.
+I am sorry, I cannot answer this question.
+"""
